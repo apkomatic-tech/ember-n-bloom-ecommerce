7d8c4d91b9d6f07b7a060c7b364127cc25c6c7ed
Konstantin <humbl3dev@gmail.com>
2024-06-03 09:55:00 -0700

    [feat]: product page template

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -2,29 +2,59 @@ import React from "react";
 import sampleProductImage from "@/images/tea-1.jpg";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { wixStoreServer } from "@/lib/wixStoreServer";
+import { Badge } from "@/components/ui/badge";
 
-function ProductDetailPage({ params }: { params: { slug: string } }) {
-  // TODO: product should be dynamic coming from db/cms/api
-  const product = {
-    name: "Classic English Breakfast",
-    description:
-      "A robust morning tea blend to jumpstart your day. Full-bodied with malty notes and a hint of citrus.",
-    price: 12.99,
-    type: "black",
-    image: sampleProductImage,
-  };
+async function getProductDetailsBySlug(slug: string) {
+  const wixStore = await wixStoreServer();
+  const product = await wixStore.products
+    .queryProducts()
+    .eq("slug", slug)
+    .limit(1)
+    .find();
+  return product;
+}
+
+async function ProductDetailPage({ params }: { params: { slug: string } }) {
+  const productData = await getProductDetailsBySlug(params.slug);
+  const [product] = productData.items;
+  const { name, description, media, price, ribbons } = product;
+  const productImageUrl = media?.mainMedia?.image?.url ?? "";
+  const productImageWidth = media?.mainMedia?.image?.width ?? 0;
+  const productImageHeight = media?.mainMedia?.image?.height ?? 0;
+  const productPriceFormatted = price?.formatted?.price;
   return (
-    <div className="grid gap-8 px-4 md:mt-16 md:grid-cols-2 md:gap-16 lg:gap-32 2xl:px-0">
+    <div className="mt-4 grid gap-8 px-4 md:grid-cols-2 md:gap-16 lg:gap-32 2xl:px-0">
       {/* Left column (image) */}
       <div>
-        <Image src={product.image} alt={product.name} className="rounded-md" />
+        <Image
+          src={productImageUrl}
+          width={productImageWidth}
+          height={productImageHeight}
+          alt={product.name || ""}
+          className="rounded-md"
+        />
       </div>
       {/* Right column (details) */}
       <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-bold lg:text-3xl">{product.name}</h1>
-        <p className="text-xl">${product.price}</p>
-        <p className="text-black/60">{product.description}</p>
-        <Button className="w-full md:w-1/2" variant={"secondary"}>
+        {ribbons && ribbons.length > 0 && (
+          <div className="flex gap-2">
+            {ribbons.map((ribbon) => (
+              <Badge variant={"secondary"} key={ribbon.text}>
+                {ribbon.text}
+              </Badge>
+            ))}
+          </div>
+        )}
+        <h1 className="text-2xl font-bold lg:text-3xl">{name}</h1>
+        <p className="text-xl">{productPriceFormatted}</p>
+        {description && (
+          <p
+            className="text-black/60"
+            dangerouslySetInnerHTML={{ __html: description }}
+          ></p>
+        )}
+        <Button className="w-full md:w-1/2" variant={"default"}>
           Add To Cart
         </Button>
       </div>
